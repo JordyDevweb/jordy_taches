e3df0e68f37e6fcb9f94775a3b819fa132570fbd
Balou <baloujordy28@gmail.com>
2026-07-02 22:41:32 +0200

    Correction onglet inscription et database cloud

--- database.py
+++ database.py
@@ -1,14 +1,23 @@
+import streamlit as st
 import mysql.connector
 from mysql.connector import Error
+
+
+def get_secret(cle, valeur_locale):
+    try:
+        return st.secrets["mysql"].get(cle, valeur_locale)
+    except Exception:
+        return valeur_locale
 
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="jordy_taches"
+            host=get_secret("host", "localhost"),
+            user=get_secret("user", "root"),
+            password=get_secret("password", ""),
+            database=get_secret("database", "jordy_taches"),
+            port=int(get_secret("port", 3306))
         )
         return connection
 
